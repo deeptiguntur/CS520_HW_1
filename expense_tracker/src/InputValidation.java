058597dd55e23a939899486514b0fb416082e511
Deepti Guntur <deepti25.guntur@gmail.com>
2023-09-26 12:25:16 -0400

    Adding amount and category validation functions

---
+++ expense_tracker/src/InputValidation.java
@@ -0,0 +1,24 @@
+
+public class InputValidation {
+
+    public enum Category {FOOD, TRAVEL, BILLS, ENTERTAINMENT, OTHER}
+
+    /**
+     * Validate amount input
+     * @param amount - the amount to entered for transaction
+     * @throws IllegalArgumentException - amount should be a number and greater than 0 and less than 1000
+     */
+    public double validateAmount(String amountInput) {
+        double amount = 0;
+        return amount;
+    }
+
+    /**
+     * Validate amount input
+     * @param amount - the amount to entered for transaction
+     * @throws IllegalArgumentException - amount should be a number and greater than 0 and less than 1000
+     */
+    public boolean validateCategory(String amount) {
+        return true;
+    }
+}
